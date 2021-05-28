f24b335e0e6db976a1fe2b5d623b550b6e1c97db
prafull <jain.pj2310@gmail.com>
2021-05-28 11:17:22 +0530

    make it third

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Root app component
+          My first react js app
         </p>
       </header>
     </div>
