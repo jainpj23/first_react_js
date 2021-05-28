bdbbcd13338628671be63c88a436887c400a3101
prafull <jain.pj2310@gmail.com>
2021-05-28 10:43:14 +0530

    make it second

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Root APP Component
+          Root aPP component
         </p>
       </header>
     </div>
